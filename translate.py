5b1c3f7e6ac5be485f2fd11b2166dfa42bef426a
Spurthi Madhukar <spurthimadhukar@gmail.com>
2025-07-12 17:35:17 +0530

    Update translate.py

--- translate.py
+++ translate.py
@@ -5,21 +5,21 @@ translated = []
 with open("titles.txt", "r", encoding="utf-8") as f:
     titles = f.readlines()
 
+titles = [t.strip() for t in titles if t.strip()] 
+
 if not titles:
-    print("titles.txt is empty. Nothing to translate.")
+    print("titles.txt is empty. Nothing to translate")
 else:
-    print(f"Found {len(titles)} titles to translate...")
+    print(f"Found {len(titles)} titles to translate")
 
-
-for t in titles:
+for i, t in enumerate(titles):
     try:
-        translated_title = GoogleTranslator(source='auto', target='en').translate(t.strip())
-        print(f"→ {translated_title}")
+        translated_title = GoogleTranslator(source='auto', target='en').translate(t)
+        print(f"{i+1}. → {translated_title}")
         translated.append(translated_title)
     except Exception as e:
         print("Translation failed:", e)
         translated.append("[Translation failed]")
-
 
 with open("translated_titles.txt", "w", encoding="utf-8") as f:
     for line in translated:
